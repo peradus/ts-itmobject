e1ecd3fb374a011c94ff58c38fdb0adcba5082fa
Patrick Eradus <p.eradus@amc.nl>
2018-05-04 14:30:58 +0200

    webpack test

--- ts/class/itmobject.ts
+++ ts/class/itmobject.ts
@@ -3,273 +3,80 @@
 ///<reference path='./itmobjectinstances.ts'/>
 
 class ItmObject  {
-
-   protected _instances:ItmObjectInstances;
-   protected _methods:ItmObjectMethods;
-   protected _properties:ItmObjectProperties;
+   
+   private _name:string;
+   private _classname:string;
+   private _displayname:string;
+   private _description:string;
+   private _instances:ItmObjectInstances;
+   private _methods:ItmObjectMethods;
+   private _properties:ItmObjectProperties;
 
    constructor (name:string)
    {
+         this._name=name;
+         this._classname="itmobject";
+         this._displayname=this._name;
+         this._description="";
          this._methods=new ItmObjectMethods();
          this._properties=new ItmObjectProperties();
          this._instances=new ItmObjectInstances();
 
-         this.setPropertyValue('_name',name);
-         this.setPropertyValue('_className','itmobject');
-         this.setPropertyValue('_displayname',name);
-         this.setPropertyValue('_description','');
-         this.setPropertyValue('_status','ok');
-      }
+         this._properties.set(
+            new ItmObjectProperty('className').setValue('itmobjectclass')
+         );
+   }
 
-   
-   get instances():ItmObjectInstances   {
+   public instances():ItmObjectInstances
+   {
       return this._instances;
    }
    
-   get properties():ItmObjectProperties {
-      return this._properties;
-   }
-
-   get methods():ItmObjectMethods    {
-      return this._methods;
+   // GET ITMOBJECT BASED ON INSTANCENAME
+   public getItmObject(instance:string):ItmObject | undefined
+   {
+      if (instance == "") {
+         // return this itmobject
+         return this;
+      }
+      else {
+         let instanceArray:string[] = instance.split('/');
+         let findInstance:string | undefined;
+         
+         findInstance=instanceArray.shift();
+         if (findInstance) 
+         {
+            if (this._instances.exist(findInstance))
+            {
+               let obj:ItmObject=this._instances.get(findInstance);
+               if (obj) 
+               {
+                  let nextInstance:string=instanceArray.join('/');
+                  return obj.getItmObject(nextInstance);
+               }
+            }
+         }
+      }
+      return undefined;
    }
 
    // LOCAL ITMOBJECT METHODS
-   /**
-    * Get ITMObject property value
-    * @param property 
-    */
-   public getPropertyValue(property:string):string  {
-      return this._properties.getValue(property);
-   }
-
-   /**
-    * Set ITMObject property value
-    * @param property - property to set
-    * @param value    - value to set 
-    */
-   public setPropertyValue(property:string, value:string):string  {
-      return this._properties.setValue(property, value);
-   }
-
-   /**
-    * Get ITMObject name
-    */
-   public getName():string {
-      return this.getPropertyValue('_name');
-
-   }
-
-   /**
-    * Get ITMObject classname
-    */
-      
-   public getClassName():string    {
-      return this.getPropertyValue('_classname');
-   }
-   
-   /**
-    * Get ITMObject displayname
-    */
-   public getDisplayName():string {
-      return this.getPropertyValue('_displayname');
-   }
-   
-   /**
-    * Get ITMObject status
-    */
-   public getStatus():string {
-      return this.getPropertyValue('_status');
-   }
-
-   /**
-    * Get ITMObject description
-    */
-   public getDescription():string {
-      return this.getPropertyValue('_description');
-   }
-
-   /**
-    * Get ITMObject properties
-    */
-   public getProperties():string {
-      return this._properties.toString(); 
-   }
-
-   /**
-    * Get ITMObject methods, returns comma-separated method list
-    */
-   public getMethods():string {
-      return this._methods.toString(); 
-   }
-
-   /**
-    * Do ITMObject method, returns status after method execution
-    * @param method 
-    */
-   public doMethod(method:string):string {
-      return "doMethod: result=["+method+"]";
+   public getName():string 
+   {
+      return this._name;
    }
 
    // INSTANCE REFERENCED METHODS
-   /**
-    * Get ITMObject instances, returns comma-separated instance list
-    * @param instance - select instance
-    */
-   public getInstances(instance:string=""):string {
-      if (instance=="") return this._instances.toString();
-      
-      let instanceArray:string[] = instance.split('/');
-      let findInstance:string | undefined=instanceArray.shift();
-
+   public getInstanceName(instance:string):string
+   {
       let obj:ItmObject | undefined;
-      obj=this._instances.get(findInstance);
+      obj=this.getItmObject(instance);
       if (obj) 
-      {  let nextInstance:string = instanceArray.join('/');
-         return obj.getInstances(nextInstance);
+      {
+         return obj.getName();
       }
-      return "";
-   }
-   
-   /**
-    * Get ITMObject selected instance property value
-    * @param instance - instance select
-    * @param property - property select
-    */
-   public getInstancePropertyValue(instance:string, property:string):string {
-      if (instance=="") return this.getPropertyValue(property);
-      
-      let instanceArray:string[] = instance.split('/');
-      let findInstance:string | undefined=instanceArray.shift();
-
-      let obj:ItmObject | undefined;
-      obj=this._instances.get(findInstance);
-      if (obj) 
-      {  let nextInstance:string = instanceArray.join('/');
-         return obj.getInstancePropertyValue(nextInstance, property);
+      else {
+         return "";
       }
-
-      return "";
-   }
-
-   /**
-    * Set ITMObject selected instance property value
-    * @param instance - instance select
-    * @param property - property select
-    * @param value    - set value
-    */
-   public setInstancePropertyValue(instance:string, property:string, value:string):string {
-      if (instance=="") return this.setPropertyValue(property, value);
-      
-      let instanceArray:string[] = instance.split('/');
-      let findInstance:string | undefined=instanceArray.shift();
-
-      let obj:ItmObject | undefined;
-      obj=this._instances.get(findInstance);
-      if (obj) 
-      {  let nextInstance:string = instanceArray.join('/');
-         return obj.setInstancePropertyValue(nextInstance, property, value);
-      }
-
-      return "";
-   }
-
-   /**
-    * Get ITMObject selected instance properties
-    * @param instance - instance select
-    */
-   public getInstanceProperties(instance:string):string {
-      if (instance=="") return this.getProperties();
-      
-      let instanceArray:string[] = instance.split('/');
-      let findInstance:string | undefined=instanceArray.shift();
-
-      let obj:ItmObject | undefined;
-      obj=this._instances.get(findInstance);
-      if (obj) 
-      {  let nextInstance:string = instanceArray.join('/');
-         return obj.getInstanceProperties(nextInstance);
-      }
-      return "";
-   }
-
-   /**
-    * Get ITMObject selected instance methods
-    * @param instance - instance select
-    */
-   public getInstanceMethods(instance:string):string {
-      if (instance=="") return this.getMethods();
-      
-      let instanceArray:string[] = instance.split('/');
-      let findInstance:string | undefined=instanceArray.shift();
-
-      let obj:ItmObject | undefined;
-      obj=this._instances.get(findInstance);
-      if (obj) 
-      {  let nextInstance:string = instanceArray.join('/');
-         return obj.getInstanceMethods(nextInstance);
-      }
-      return "";
-   }
-
-   /**
-    * Do ITMObject method, returns result of method
-    * @param instance - instance select
-    * @param method - method select
-    */
-   public doInstanceMethod(instance:string, method:string):string {
-      if (instance=="") return this.doMethod(method);
-      
-      let instanceArray:string[] = instance.split('/');
-      let findInstance:string | undefined=instanceArray.shift();
-
-      let obj:ItmObject | undefined;
-      obj=this._instances.get(findInstance);
-      if (obj) 
-      {  let nextInstance:string = instanceArray.join('/');
-         return obj.doInstanceMethod(nextInstance,method);
-      }
-      return "";
-   }
-
-   /**
-    * Get ITMObject selected instance name
-    * @param instance - instance select
-    */
-
-   public getInstanceName(instance:string):string {
-      return this.getInstancePropertyValue(instance,'_name');
-   }
-   
-   /**
-    * Get ITMObject selected instance classname
-    * @param instance - instance select
-    */
-   public getInstanceClassName(instance:string):string {
-      return this.getInstancePropertyValue(instance,'_classname');
-   }
-   
-   /**
-    * Get ITMObject selected instance displayname
-    * @param instance - instance select
-    */
-   public getInstanceDisplayName(instance:string):string {
-      return this.getInstancePropertyValue(instance,'_displayname');
-   }
-   
-   /**
-    * Get ITMObject selected instance status
-    * @param instance - instance select
-    */
-   public getInstanceStatus(instance:string):string {
-      return this.getInstancePropertyValue(instance,'_status');
-   }
-
-   /**
-    * Get ITMObject selected instance description
-    * @param instance - instance select
-    */
-   public getInstanceDescription(instance:string):string {
-      return this.getInstancePropertyValue(instance,'_description');
    }
 }
